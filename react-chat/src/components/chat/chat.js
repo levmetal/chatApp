521375095e415bcc79344aa3ef397c3690432700
Levi oquendo <54660320+levmetal@users.noreply.github.com>
2022-11-24 21:41:08 -0500

    Update chat.js

--- react-chat/src/components/chat/chat.js
+++ react-chat/src/components/chat/chat.js
@@ -18,7 +18,7 @@ const Chat =({location})=>{
 
 
 
-  const ENDPOINT ="https://chat-react-s.herokuapp.com"   ///////////////endpoint del servidor////////////////////////////////////////////////
+  const ENDPOINT ="https://serverchat.onrender.com/"   ///////////////endpoint del servidor////////////////////////////////////////////////
 
 
   useEffect(() => {
